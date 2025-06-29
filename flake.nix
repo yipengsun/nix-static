99ac215b0d3a12dcfc0d2d4104dfe9b3c2cc479f
Yipeng Sun <syp@umd.edu>
2025-06-29 23:25:12 +0800

    infra: init a flake.

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.curl;
+        }
+      );
+    };
+}
